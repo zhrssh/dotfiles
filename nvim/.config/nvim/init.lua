5925d16213494efd6c0b07ab3538a35db5674788
zhrssh <98146850+zhrssh@users.noreply.github.com>
2025-11-16 17:14:44 +0800

    Updated hypr config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -110,6 +110,9 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- No text wrap
+vim.o.wrap = false
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
