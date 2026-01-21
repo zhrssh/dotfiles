d2045e632bbf6b46f341c3fa1d1d759eb40b8a18
zhrssh <98146850+zhrssh@users.noreply.github.com>
2026-01-21 11:40:12 +0800

    Added zk plugin

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -74,4 +74,30 @@ return {
 			end, { desc = "Move to prev harpooned file" })
 		end,
 	},
+	{
+		"zk-org/zk-nvim",
+		config = function()
+			require("zk").setup({
+				-- Can be "telescope", "fzf", "fzf_lua", "minipick", "snacks_picker",
+				-- or select" (`vim.ui.select`).
+				picker = "telescope",
+
+				lsp = {
+					-- `config` is passed to `vim.lsp.start(config)`
+					config = {
+						name = "zk",
+						cmd = { "zk", "lsp" },
+						filetypes = { "markdown" },
+						-- on_attach = ...
+						-- etc, see `:h vim.lsp.start()`
+					},
+
+					-- automatically attach buffers in a zk notebook that match the given filetypes
+					auto_attach = {
+						enabled = true,
+					},
+				},
+			})
+		end,
+	},
 }
