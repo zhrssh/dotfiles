82db441f0cbdafd5f11fed3dcaa6fa55fed8686f
Zherish Galvin Mayordo <98146850+zhrssh@users.noreply.github.com>
2026-02-24 21:22:11 +0800

    Removed gitleaks and codeql

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -661,11 +661,9 @@ require("lazy").setup({
 			vim.list_extend(ensure_installed, {
 				"bandit",
 				"bash-language-server",
-				"codeql",
 				"dockerfile-language-server",
 				"eslint-lsp",
 				"gh-actions-language-server",
-				"gitleaks",
 				{
 					"gofumpt",
 					condition = function()
@@ -706,13 +704,6 @@ require("lazy").setup({
 						require("lspconfig")[server_name].setup(server)
 					end,
 				},
-			})
-
-			-- Autocmd
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					vim.fn.jobstart("gitleaks detect --source " .. vim.fn.getcwd())
-				end,
 			})
 		end,
 	},
